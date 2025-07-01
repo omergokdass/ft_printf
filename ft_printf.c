df0edb2e7b750ad92479bd29b4d1c6ade6c931ef
omergokdass <faruk_gokdas@hotmail.com>
2025-07-02 00:33:58 +0300

    printf

--- ft_printf.c
+++ ft_printf.c
@@ -20,17 +20,17 @@ int	ft_putchar(char c)
 int	ft_format(va_list args, char c)
 {
 	if (c == 'c')
-		return (ft_putchar(va_arg((args), int)));
+		return (ft_putchar(va_arg(args, int)));
 	else if (c == 's')
-		return (ft_putstr(va_arg((args), char *)));
+		return (ft_putstr(va_arg(args, char *)));
 	else if (c == 'd' || c == 'i')
-		return (ft_putnbr(va_arg((args), int)));
+		return (ft_putnbr(va_arg(args, int)));
 	else if (c == 'u')
-		return (ft_uint(va_arg((args), unsigned int)));
+		return (ft_uint(va_arg(args, unsigned int)));
 	else if (c == 'x' || c == 'X')
-		return (ft_hex(va_arg((args), unsigned int), c));
+		return (ft_hex(va_arg(args, unsigned int), c));
 	else if (c == 'p')
-		return (ft_point(va_arg((args), unsigned long), 1));
+		return (ft_point(va_arg(args, unsigned long), 1));
 	else if (c == '%')
 		return (ft_putchar('%'));
 	else
@@ -48,7 +48,7 @@ int	ft_printf(const char *str, ...)
 	va_start(args, str);
 	while (str[i])
 	{
-		if (str[i] == '%' && str[i + 1])
+		if (str[i] == '%' && str[i + 1] != '\0')
 		{
 			len += ft_format(args, str[i + 1]);
 			i++;
